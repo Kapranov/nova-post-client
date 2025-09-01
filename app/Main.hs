fb39812eef783a169000f2d16df1ca834d830ff3
Oleg G.Kapranov <ok@pure-agency.co>
2025-09-01 03:42:07 -1000

    added simple http query

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
-
-import Lib
+import qualified Data.ByteString.Lazy.Char8 as L8
+import           Network.HTTP.Simple
 
 main :: IO ()
-main = someFunc
+main = do
+    response <- httpLBS "http://httpbin.org/get"
+    putStrLn $ "The status code was: " ++ show (getResponseStatusCode response)
+    print $ getResponseHeader "Content-Type" response
+    L8.putStrLn $ getResponseBody response
